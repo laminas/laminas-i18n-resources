5447daf41345c141938189188f222481094102ea
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:10:04 -0600

    Rewriting as Laminas Project package

---
+++ languages/sk/Laminas_Captcha.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-i18n-resources for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n-resources/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n-resources/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Laminas_Captcha_ReCaptcha
+    "Missing captcha fields" => "Chýba captcha pole",
+    "Failed to validate captcha" => "Captchu sa nepodarilo overiť",
+    "Captcha value is wrong: %value%" => "Hodnota captchy neni správna: %value%",
+
+    // Laminas_Captcha_Word
+    "Empty captcha value" => "Hodnota captchy nebola zadaná",
+    "Captcha ID field is missing" => "Chýba pole captcha ID",
+    "Captcha value is wrong" => "Chybná hodnota catpcha",
+);
