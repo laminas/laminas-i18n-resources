1f415756e23ecce4f3c78548ac769d36f918fdff
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:10:03 -0600

    Rewriting as Laminas Project package

---
+++ languages/no/Laminas_Captcha.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-i18n-resources for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-i18n-resources/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-i18n-resources/blob/master/LICENSE.md New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Laminas_Captcha_ReCaptcha
+    "Missing captcha fields" => "Mangler captcha felter",
+    "Failed to validate captcha" => "Validering av captcha feilet",
+    "Captcha value is wrong: %value%" => "Feil Captcha verdi for: %value%",
+
+    // Laminas_Captcha_Word
+    "Empty captcha value" => "Tomt captcha verdi",
+    "Captcha ID field is missing" => "Feltet for captcha id er ikke tilstede",
+    "Captcha value is wrong" => "Verdien til captcha er feil",
+);
